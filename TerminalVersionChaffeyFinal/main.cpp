58900c296ccdacb856d2d1fd94d9efc0d13075cc
brettmccausland <brettmccausland@yahoo.com>
2016-11-04 12:05:30 -0700

    Parsing Complete
    I removed some elements of the parser that I didn’t uses and put some
    debuggers in so i can verify things are working correctly . I am now
    going to proceed to write my addition function

--- TerminalVersionChaffeyFinal/main.cpp
+++ TerminalVersionChaffeyFinal/main.cpp
@@ -2,28 +2,25 @@
 #include<vector>
 typedef unsigned char byte;
 using namespace std;
-bool getInput(string &first, char &op, string &second, vector<string> &storage );
-
+bool getInput(string &first, string &second, char &op);
 // makes all uppercase erases all spaces
 void standardize(string &line);
-
 void reverse(string &line, vector<byte> &bignum);
-void process(string &first, char op, string &second, vector<string> & storage, char position);
-void getIndex(string& in,vector<string>& storage );
-
+void process(string &first, string &second, char op, char position);
 // remove all non numbers
 void sanitize(string &line);
 int main()
 {
+    system("clear");
     string first, second;
      vector<string> storage ;
       char op;
       char position=0;
-      while(getInput(first,op,second,storage))
-          process(first,op,second,storage,position);
+      while(getInput(first,second,op))
+          process(first,second,op,position);
       return 0;//a.exec();
 }
-
+//erase trailing spaces and leading spaces uppercase everything
 void standardize(string &line)
 {
     for(size_t i = 0; i <line.size();i++)
@@ -34,7 +31,6 @@ void standardize(string &line)
 
     while(line[line.size()-1] == ' ')
         line.erase(line.size()-1);
-
 }
 void sanitize(string &line)
 {
@@ -44,32 +40,29 @@ void sanitize(string &line)
     while(line[size = line.size()-1]<'0' || line[size] > '9')
         line.erase(size);
 }
-
 void reverse(string &line, vector<byte> &bignum)
 {
     for(int i = line.size()-1; i >= 0; --i)
+    {
         bignum.push_back( (line[i]-48));
+        cout<<(line[i]-48)<<endl;
+    }
 }
-bool getInput(string &first, char &op, string &second, vector<string> &storage)
+bool getInput(string &first, string& second, char &op)
 {
+    cout<<"in get input"<<endl;
     op=NULL;
     string line,temp,newexpression,equation;
-    bool b=false;
-    int search =0;
     string input;
     size_t pos=0;
-    size_t pos1=0;
 
     getline(cin, line);
      if(line.empty())
         return false;
-    size_t whichCommand;
-    string firstWord;
     standardize(line);
-    temp=line;
     if (line.find(',')!=std::string::npos)
         pos=line.find(',');
-    if (line.find('+')!=std::string::npos)
+    else if (line.find('+')!=std::string::npos)
         pos=line.find('+');
     else if(line.find('-')!=std::string::npos)
         pos=line.find('-');
@@ -77,35 +70,16 @@ bool getInput(string &first, char &op, string &second, vector<string> &storage)
         pos=line.find('*');
     else if(line.find('/')!=std::string::npos)
         pos=line.find('/');
+
     op = line[pos];
     first = line.substr(0,pos);
-    getIndex(first,storage);
+    second = line.substr(pos+1);
+   //clean
     sanitize(first);
+    sanitize(second);
     return true;
 }
-void getIndex(string& in,vector<string>& storage )
-{
-    string temp=in;
-    string hold="R:";
-    size_t pos=0;
-    char L ;
-    for (size_t i =0; i<in.size();i++)
-    {
-        L=in[i];
-        if(L >64 && L < 90)
-        {
-            temp=storage[L-'A'];
-            pos=temp.find(hold);
-            if(pos!=-1)
-           {
-                pos=temp.find(' ');
-                temp=temp.substr(0,pos);
-            }
-        }
-    }
-    in = temp;
-}
-void process(string &first, char op, string &second, vector<string> & storage, char position)
+void process(string &first,string& second, char op, char position)
 {
     //erase leading zeros
     while(second[0]=='0')
@@ -117,20 +91,19 @@ void process(string &first, char op, string &second, vector<string> & storage, c
 
     if(op=='+')
     {
-
+        cout<<first<<'+'<<second<<endl;
     }
     else if(op=='-')
     {
-
-
+           cout<<first<<'-'<<second<<endl;
     }
     else if(op=='*'|| op==',')
     {
-
+           cout<<first<<'*'<<second<<endl;
     }
      else if(op=='/')
     {
-
+           cout<<first<<'/'<<second<<endl;
     }
     cin.clear();
 }
